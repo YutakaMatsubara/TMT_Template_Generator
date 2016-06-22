61a0722807b3cd31ddf9a25cb1b6b079c8363ce4
Wei Jingxuan <jingxuan.w@gmail.com>
2016-06-22 16:42:29 +0900

    Add uuid variable for each element

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ t_id = str(uuid.uuid4())
 t_version = "1.0.0.2"
 t_author = "TMT Template Generator"
 
+Title_uuid = str(uuid.uuid4())
+UserThreatCategory_uuid = str(uuid.uuid4())
+UserThreatShortDescription_uuid = str(uuid.uuid4())
+UserThreatDescription_uuid = str(uuid.uuid4())
+StateInformation_uuid = str(uuid.uuid4())
+InteractionString_uuid = str(uuid.uuid4())
+Priority_uuid = str(uuid.uuid4())
+
 Automobiles_uuid = str(uuid.uuid4())
 KeyFob_uuid = str(uuid.uuid4())
 
@@ -23,25 +31,25 @@ def main():
         for node in KnowledgeBase.childNodes:
             if node.nodeName == "ThreatMetaData":
                 Title_Values = create_Values("Not Set")
-                Title = create_ThreatMetaDatum("Title", "Title", "false", Title_Values, "", str(uuid.uuid4()), "0")
+                Title = create_ThreatMetaDatum("Title", "Title", "false", Title_Values, "", Title_uuid, "0")
 
                 UserThreatCategory_Values = create_Values("")
                 UserThreatCategory = create_ThreatMetaDatum("UserThreatCategory", "Category", "false", UserThreatCategory_Values, "", "", "0")
 
                 UserThreatShortDescription_Values = create_Values("Not Set")
-                UserThreatShortDescription = create_ThreatMetaDatum("UserThreatShortDescription", "Short Description", "false", UserThreatShortDescription_Values, "", str(uuid.uuid4()), "0")
+                UserThreatShortDescription = create_ThreatMetaDatum("UserThreatShortDescription", "Short Description", "false", UserThreatShortDescription_Values, "", UserThreatShortDescription_uuid, "0")
 
                 UserThreatDescription_Values = create_Values("Not Set")
-                UserThreatDescription = create_ThreatMetaDatum("UserThreatDescription", "Description", "false", UserThreatDescription_Values, "", str(uuid.uuid4()), "0")
+                UserThreatDescription = create_ThreatMetaDatum("UserThreatDescription", "Description", "false", UserThreatDescription_Values, "", UserThreatDescription_uuid, "0")
 
                 StateInformation_Values = create_Values("Not Set")
-                StateInformation = create_ThreatMetaDatum("StateInformation", "Justification", "false", StateInformation_Values, "", str(uuid.uuid4()), "0")
+                StateInformation = create_ThreatMetaDatum("StateInformation", "Justification", "false", StateInformation_Values, "", StateInformation_uuid, "0")
 
                 InteractionString_Values = create_Values("Not Set")
-                InteractionString = create_ThreatMetaDatum("InteractionString", "Interaction", "false", InteractionString_Values, "", str(uuid.uuid4()), "0")
+                InteractionString = create_ThreatMetaDatum("InteractionString", "Interaction", "false", InteractionString_Values, "", InteractionString_uuid, "0")
 
                 Priority_Values = create_Values("High", "Medium", "Low")
-                Priority = create_ThreatMetaDatum("Priority", "Priority", "false", Priority_Values, "Priority", str(uuid.uuid4()), "1")
+                Priority = create_ThreatMetaDatum("Priority", "Priority", "false", Priority_Values, "Priority", Priority_uuid, "1")
 
                 PropertiesMetaData = create_PropertiesMetaData(Title, UserThreatCategory, UserThreatDescription, StateInformation, InteractionString, Priority)
                 ThreatMetaData_new = create_ThreatMetaData("true", "true", PropertiesMetaData)
@@ -95,9 +103,9 @@ def main():
                 GenerationFilters1 = create_GenerationFilters(Include1, "")
 
                 UserThreatDescription_Values = create_Values("{flow.Name} will not be initiated between {source.Name} and {target.Name}.")
-                UserThreatDescription = create_ThreatMetaDatum("UserThreatDescription", "Description", "false", UserThreatDescription_Values, "", str(uuid.uuid4()), "0")
+                UserThreatDescription = create_ThreatMetaDatum("UserThreatDescription", "Description", "false", UserThreatDescription_Values, "", UserThreatDescription_uuid, "0")
                 Priority_Values = create_Values("High", "Medium", "Low")
-                Priority = create_ThreatMetaDatum("Priority", "Priority", "false", Priority_Values, "Priority", str(uuid.uuid4()), "1")
+                Priority = create_ThreatMetaDatum("Priority", "Priority", "false", Priority_Values, "Priority", Priority_uuid, "1")
                 PropertiesMetaData = create_PropertiesMetaData(UserThreatDescription, Priority)
                 ThreatType = create_ThreatType(GenerationFilters1, str(uuid.uuid4()), "Service Omitted", Omission_uuid, "", "{flow.Name} will not be initiated between {source.Name} and {target.Name}.", PropertiesMetaData)
                 node.appendChild(ThreatType)
